060f488a1b8171ac7581e75df75caec486b8f92b
Holly Parker <hollygcochran@gmail.com>
2022-06-26 16:09:33 -0500

    created arrays for characters

---
+++ script.js
@@ -0,0 +1,11 @@
+// prompt >dialog box
+// 8-128 chars
+// prompt for uppercase, lowercase, numeric, and/or special characters
+
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const specialChars = ["!", "#", "$", "%", "&", "*", "_", "=", "~", "?"]
