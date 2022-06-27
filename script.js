967fabab71ea8e5252ec4c4b57d54aeb86445774
Holly Parker <hollygcochran@gmail.com>
2022-06-27 12:53:50 -0500

    adding logic to generate password

--- script.js
+++ script.js
@@ -1,30 +1,123 @@
 // prompt >dialog box
 // 8-128 chars
 // prompt for uppercase, lowercase, numeric, and/or special characters
+var generatePassBtn = document.querySelector("#generate");
 
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const specialChars = ["!", "#", "$", "%", "&", "*", "_", "=", "~", "?"]
+const specialChars = ["!", "#", "$", "%", "&", "*", "_", "=", "~", "?"];
 
-console.log(upperCase)
-console.log(lowerCase)
-console.log(numbers)
-console.log(specialChars)
+console.log(generatePassBtn);
+console.log(upperCase);
+console.log(lowerCase);
+console.log(numbers);
+console.log(specialChars);
 
-var length = prompt ("Please select a password length between 8 and 128 characters.")
+function passwordGenerator() {
+  console.log("inside");
+  var length = prompt(
+    "Please select a password length between 8 and 128 characters."
+  );
+  if (length < 8 || length > 128) {
+    alert("Please select a password length between 8 and 128 characters.");
+    return null;
+  } else {
+    var isUpperCase = confirm("Include uppercase characters?");
+    var isLowerCase = confirm("Include lowercase characters?");
+    var isNumbers = confirm("Include numbers?");
+    var isSpecialChars = confirm("Include special characters?");
+    console.log(isUpperCase)
+    console.log(isLowerCase)
+    console.log(isNumbers)
+    console.log(isSpecialChars)
+    
+    var passwordPool = "" 
+    if (isUpperCase) {
+        passwordPool += upperCase
+    }
+    if (isLowerCase) {
+        passwordPool += lowerCase
+    }
+    if (isNumbers) {
+        passwordPool += numbers
+    }
+    if (isSpecialChars) {
+        passwordPool += specialChars
+    }
+    console.log(passwordPool)
 
-var isUpperCase = confirm("Include uppercase characters?")
+    var finalPassword = "";
 
-var isLowerCase = confirm ("Include lowercase characters?")
+    for (let i = 0; i < length; i++) {
+        finalPassword += passwordPool[Math.floor(Math.random() * passwordPool.length)]
+        console.log(i)
+        console.log(i + " time in for loop " +finalPassword)        
+    }
+  }
+}
 
-var isNumbers = confirm ("Include numbers?")
+// need maybe two functions:
+// one for generating random number
+// one for handling answers from user
 
-var isSpecialChars = confirm("Include special characters?")
+// refer to math.random
 
-// need maybe two functions: 
-    // one for generating random number
-    // one for handling answers from user
+generatePassBtn.addEventListener("click", passwordGenerator);
